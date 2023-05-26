cb99041ddabe80d297ed9aacf7d71d122c55b522
Timur Safarov <tisafarov@gmail.com>
2023-05-26 13:50:37 +0400

    Update 26/05/2023

--- connect.php
+++ connect.php
@@ -1,6 +1,18 @@
 <?php
-$user='root';
-$password='root';
-$host='localhost';
-$db='registration';
-$dbh = new PDO("mysql:host=$host;dbname=$db",$user,$password);
+
+$user = 'root';
+$password = 'root';
+$host = 'localhost';
+$db = 'registration';
+
+//Блок try catch проверяет подключение к базе
+try {
+	//Если норм то работаем дальше
+	//$dbh - это наша ссылка на подключение к БАЗЕ
+    $dbh = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+} catch(PDOException $e) {
+	//Иначе выводим ошибяку
+    echo $e->getMessage();
+}
+
+
--- reg.php
+++ reg.php
@@ -1,20 +1,47 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_startup_errors', 0);
-ini_set('display_errors','On');
+ini_set('display_errors', 'On');
 
+//Подключаемся к базе через файл connect.php
+require ('./connect.php');
 
-require ('connect.php');
-$login = $_POST['login'];
-$email = $_POST['email'];
-if ($login==''){
-	echo 'ведите логин';
+$login = @$_POST['login'];
+$email = @$_POST['email'];
+
+//Если пустая переменная login тогда говорим ввести логин
+//Если пустой Email тогда говорим ввести Email
+if (empty($login)) {
+	die('ведите логин');
+} elseif(empty($email)) {
+	die('Введите email');
 }
-elseif($email==''){
-	echo 'Введите email';
+
+try {
+
+	//Генерируем пароль так как мы не указали его в форме
+	//md5 превращает наш пароль в хэш так как пароль в базе нельзя хранить в исходном виде
+	//только в зашифрованном
+	$password = md5(random_bytes(10));
+
+    //Сохраняем запрос в переменную $sql
+	$sql = "INSERT INTO users(`login`, `email`, `password`) VALUES('$login', '$email', '$password')";
+
+    //Получаем количество выполненных запросов
+    $res = $dbh->exec($sql);
+
+   	//Проверяем работает запрос или нет
+    if ($res) {
+    	echo 'Вы зарегистрировались под номером #' . $res;
+    } else {
+    	//Если нет то выводим ошибку
+    	//$dbh->errorInfo() - это массив с ошибками
+    	throw new Exception('Запрос не выполнен - ' . print_r($dbh->errorInfo(), true));
+    }
+    
+} catch(PDOException $e) {
+	//В этот блок у нас попадают ошибки
+    echo $e->getMessage();
 }
 
-echo $sql="INSERT INTO users(login,email) VALUES($login,$email)";
-//$dbh->exec($sql);
 
-echo "Вы зарегистрировались";
